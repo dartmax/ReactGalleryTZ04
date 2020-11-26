9521ba1e4dab795c88ab4fb19b50fc7098474d5a
dartmax <promostend@gmail.com>
2020-11-26 22:12:51 +0200

    new changes for api

--- src/api/getImages-api.js
+++ src/api/getImages-api.js
@@ -3,6 +3,6 @@ import instance from './api';
 
 export const getImagesAPI = {
   getImage(data) {
-    return instance.get(`images/`, data).then(res => res.data);
+    return instance.get(`images/` + data).then(res => res.data);
   }
 };
