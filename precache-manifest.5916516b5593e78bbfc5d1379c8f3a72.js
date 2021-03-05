0fbae27d28c2328c901acb64a5fbc7f99232afad
dartmax <promostend@gmail.com>
2021-03-05 22:02:03 +0200

    Updates

---
+++ precache-manifest.5916516b5593e78bbfc5d1379c8f3a72.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa836962ad969c3bf74c5057d1438701",
+    "url": "/reactGalleryTZ04/index.html"
+  },
+  {
+    "revision": "969517918b7efae6d433",
+    "url": "/reactGalleryTZ04/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f882e91be4e5a1e2170f",
+    "url": "/reactGalleryTZ04/static/css/main.04516a44.chunk.css"
+  },
+  {
+    "revision": "969517918b7efae6d433",
+    "url": "/reactGalleryTZ04/static/js/2.a759d90d.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/reactGalleryTZ04/static/js/2.a759d90d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f882e91be4e5a1e2170f",
+    "url": "/reactGalleryTZ04/static/js/main.80b9d21a.chunk.js"
+  },
+  {
+    "revision": "4853efc8428d0782ce03",
+    "url": "/reactGalleryTZ04/static/js/runtime-main.34bc98ff.js"
+  },
+  {
+    "revision": "59130dc98a6a1a2e79bf352b9d4273fe",
+    "url": "/reactGalleryTZ04/static/media/list.59130dc9.svg"
+  },
+  {
+    "revision": "95f81f823adb4fe596de1cdfb463b9e5",
+    "url": "/reactGalleryTZ04/static/media/userImages.95f81f82.png"
+  }
+]);
