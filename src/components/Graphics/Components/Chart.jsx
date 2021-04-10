2b8bba0b52a1841e6ac7f36b81968e925d1d433e
dartmax <promostend@gmail.com>
2021-04-10 03:12:01 +0300

    T0002 Add Graphic canvas

--- src/components/Graphics/Components/Chart.jsx
+++ src/components/Graphics/Components/Chart.jsx
@@ -8,10 +8,80 @@ const PADDING = 40
 const DPI_WIDTH = WIDTH * 2
 const DPI_HEIGHT = HEIGHT * 2
 const VIEW_HEIGHT = DPI_HEIGHT - PADDING * 2
+const VIEW_WIDTH = DPI_WIDTH
 const ROWS_COUNT = 5
+
 
 const Chart = ({data}) => {
   const canvasRef = useRef(null)
+  console.log('getCharData', data);
+
+  function line(ctx, coords, {color}){
+    ctx.beginPath()
+    ctx.lineWidth = 4
+    ctx.strokeStyle = color
+    for (const [x, y] of coords) {
+      ctx.lineTo(x, y)
+    }
+    ctx.stroke()
+    ctx.closePath()
+  }
+
+  function yAxis(ctx, yMin, yMax) {
+    const step = VIEW_HEIGHT / ROWS_COUNT
+    const textStep = (yMax - yMin) / ROWS_COUNT
+    ctx.beginPath()
+    ctx.strokeStyle = '#bbb'
+    ctx.font = 'normal 20px Helvetica,sans-serif'
+    ctx.fillStyle = '#96a2aa'
+    for (let i = 1; i <= ROWS_COUNT; i++) {
+      const y = step * i
+      const text = Math.round(yMax - textStep * i)
+      ctx.fillText(text.toString(), 5, y + PADDING - 10)
+      ctx.moveTo(0, y + PADDING)
+      ctx.lineTo(DPI_WIDTH, y + PADDING)
+    }
+    ctx.stroke()
+    ctx.closePath()
+  }
+
+  function xAxis(ctx, data, xRatio) {
+    const colsCount = 6
+    const step = Math.round(data.length/colsCount)
+    ctx.beginPath()
+    for (let i = 1; i< data.length; i += step){
+      const text = toDate(data[i])
+      const x = i * xRatio
+        ctx.fillText(text.toString(), x, DPI_HEIGHT - 10)
+    }
+    ctx.closePath()
+  }
+
+  function toCoords(xRatio, yRatio) {
+    return (col) => col.map((y, i) => [
+      Math.floor((i - 1) * xRatio),
+      Math.floor(DPI_HEIGHT - PADDING - y * yRatio),
+    ]).filter((_, i) => i !== 0)
+  }
+
+  function toDate(timestamp) {
+    const shortMonths = [
+      'Jan',
+      'Feb',
+      'Mar',
+      'Apr',
+      'May',
+      'Jun',
+      'Jul',
+      'Aug',
+      'Sep',
+      'Oct',
+      'Nov',
+      'Dec',
+    ]
+    const date = new Date(timestamp)
+    return `${shortMonths[date.getMonth()]} ${date.getDate()}`
+  }
 
 
   useEffect(() => {
@@ -23,35 +93,27 @@ const Chart = ({data}) => {
     canvas.height = DPI_HEIGHT
     const [yMin, yMax] = computeBoundaries(data)
     const yRatio = VIEW_HEIGHT / (yMax - yMin)
-    // === y axis
-    const step = VIEW_HEIGHT / ROWS_COUNT
-    const textStep = (yMax - yMin) / ROWS_COUNT
-    ctx.beginPath()
-    ctx.strokeStyle = '#bbb'
-    ctx.font = 'normal 20px Helvetica,sans-serif'
-    ctx.fillStyle = '#96a2aa'
-      for (let i = 1; i <= ROWS_COUNT; i++) {
-        const y = step * i
-        const text = yMax - textStep * i
-        ctx.fillText(text.toString(), 5, y + PADDING - 10)
-        ctx.moveTo(0, y + PADDING)
-        ctx.lineTo(DPI_WIDTH, y + PADDING)
-      }
-    ctx.stroke()
-    ctx.closePath()
-    // ==
+    const xRatio = VIEW_WIDTH / (data.columns[0].length - 2)
 
-    ctx.beginPath()
-    ctx.lineWidth = 4
-    ctx.strokeStyle = '#ff0000'
-    for (const [x, y] of data) {
-      ctx.lineTo(x, DPI_HEIGHT - PADDING - y * yRatio)
-    }
-    ctx.stroke()
-    ctx.closePath()
-  }, [WIDTH, HEIGHT])
+    const yData = data.columns.filter((col) => data.types[col[0]] === 'line')
+    const xData = data.columns.filter((col) => data.types[col[0]] === 'line')[0]
 
 
+    yAxis(ctx, yMin, yMax)
+    xAxis(ctx, xData, xRatio)
+
+
+    yData.map(toCoords(xRatio, yRatio)).forEach((coords, idx) => {
+      const color = data.colors[yData[idx][0]]
+      line(ctx, coords, {color})
+      console.log(coords)
+    })
+
+
+  }, [data])
+
+
+debugger;
   return <canvas ref={canvasRef} {...data}/>
 }
 
--- src/components/Graphics/Components/Helpers.js
+++ src/components/Graphics/Components/Helpers.js
@@ -1,13 +1,22 @@
-export const computeBoundaries = (data) => {
+export const computeBoundaries = ({columns, types}) => {
   let min
   let max
 
-  for (const [, y] of data) {
-    if(typeof min !== 'number') min = y
-    if(typeof max !== 'number') max = y
+  columns.forEach(col => {
+    if(types[col[0]] !== 'line') {
+      return;
+    }
+      if(typeof min !== 'number') min = col[1]
+      if(typeof max !== 'number') max = col[1]
 
-    if(min > y) min = y
-    if(max < y) max = y
-  }
+      if(min > col[1]) min = col[1]
+      if(max < col[1]) max = col[1]
+
+    for(let i = 2; i<col.length; i++){
+      if(min > col[i]) min = col[i]
+      if(max < col[i]) max = col[i]
+    }
+  })
+
   return [min, max]
 }
