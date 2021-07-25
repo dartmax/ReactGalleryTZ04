85a4d0ff3d950d51045fc8e45a64f03227cd534f
Max.KievSky <promostend@gmail.com>
2021-07-25 15:32:05 +0300

    Update TimeArrow.js

--- src/components/TimeArrow.js
+++ src/components/TimeArrow.js
@@ -2,7 +2,13 @@ import React from "react";
 
 const TimeArrow = React.forwardRef(({type, angle, handleHover, memoHandlerHover, memoHandlerHoverHour, value}, ref) => { // forwardRef - чтобы не оборачивать компонент лишними врапперами - span. div и тд, мы используем этот хук
   return(
-    <div onMouseEnter={() =>{handleHover && handleHover(value) && memoHandlerHover(value) && memoHandlerHoverHour(value)}} ref={ref} style={{transform: `rotate(${angle}deg)`}} className={`time-arrow-wrapper-${type}`}>  {/*и передаем ref в верхний елемент*/}
+    <div 
+      onMouseEnter={() => {
+        handleHover && handleHover(value) && memoHandlerHover(value) && memoHandlerHoverHour(value)}}
+      ref={ref}
+      className={`time-arrow-wrapper-${type}`}
+      style={{transform: `rotate(${angle}deg)`}} 
+    >{/*и передаем ref в верхний елемент*/}
       <div className={`time-arrow-${type}`}/>
     </div>
   )
